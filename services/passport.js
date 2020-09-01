2bb17b9cb139ab1dbccde37e3b4c34ea411e6daa
David Munsell <david_munsell@yahoo.com>
2020-09-01 16:50:10 -0500

    Updated index.js and added logic to passport.js to find or create a new user as required for Google OAUTH

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -10,7 +13,14 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // existing user matched by profile id
+        } else {
+          // no match on profile id so create a new User
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
