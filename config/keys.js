63d5901fed188bc1143e96140063c36410688467
David Munsell <david_munsell@yahoo.com>
2020-09-01 15:25:38 -0500

    Adding config and models folder.

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "922974926672-vb51toa3k2qambubaga40ahinfsphum3.apps.googleusercontent.com",
+  googleClientSecret: "_CjwXXaCNti36Sy37aia9RHz",
+  mongoURI:
+    "mongodb+srv://admin:KIMv76KCUYtx6Ple@cluster0.gbezb.mongodb.net/ReactEmail?retryWrites=true&w=majority",
+};
