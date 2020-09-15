cb386ce326a4ee1370fdb07249f8b4e5ed573ca3
David Munsell <david_munsell@yahoo.com>
2020-09-15 12:54:22 -0500

    Adding untracked files.

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+  googleClientID:
+    "922974926672-vb51toa3k2qambubaga40ahinfsphum3.apps.googleusercontent.com",
+  googleClientSecret: "_CjwXXaCNti36Sy37aia9RHz",
+  mongoURI:
+    "mongodb+srv://admin:KIMv76KCUYtx6Ple@cluster0.gbezb.mongodb.net/ReactEmail?retryWrites=true&w=majority",
+  cookieKey: "opngbwoapngownigawpo382",
+};
+
+//o8djZkKxVIQQBEwz
+
+//mongodb+srv://admin:o8djZkKxVIQQBEwz@reactprod.wvhlw.gcp.mongodb.net/ReactEmailProd?retryWrites=true&w=majority
