498b6f0d4dc3c9576938e2175107d6c39cf2b1ea
David Munsell <david_munsell@yahoo.com>
2020-12-10 13:34:06 -0600

    Adding building folder to try and fix Heroku build errors.

---
+++ client/build/precache-manifest.46d0306e7a52ae041b1bdfd545d20ad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a552ff731b9baec109b0346d7541fbe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ce02fd29a7307f24258",
+    "url": "/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "6ce02fd29a7307f24258",
+    "url": "/static/js/2.68780e88.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.68780e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae71bac61b2fabea6fa2",
+    "url": "/static/js/main.bd9875d5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
